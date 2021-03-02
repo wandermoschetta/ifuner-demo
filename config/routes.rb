a4eb23c9ffb17880fe90ce63eab026bccb775079
wandermoschetta <wander.sm@gmail.com>
2021-03-02 00:08:15 -0300

    setup devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new'
     get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
   end
 
   
